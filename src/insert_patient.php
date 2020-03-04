d7d7ec667ccf78694fc4610d0ee12220f06efe85
Nwabunor Jeff Onwuanyi <elite057@gmail.com>
2020-03-04 16:57:02 -0700

    ajustments

--- src/insert_patient.php
+++ src/insert_patient.php
@@ -1,12 +1,12 @@
 <html>
-<head><title> Hospital Database ** Add New Peatient </title></head>
+<head><title> Hospital Database ** Add New Patient </title></head>
   <body>
-    <h2> Add a new person </h2>
+    <h2> Add a new Patient </h2>
     <from action = "insertpatient.php" method = post>
-      Person Id: <input type = text name = "id" size = 6><br><br>
-      Person Name: <input type = text name = "name" size = 20><br><br>
-      Person Address: <input type = text name = "address" size = 20><br><br>
-      Person Dob: <input type = text name = "dob" size = 20><br><br>
+      Patient Id: <input type = text name = "id" size = 6><br><br>
+      Patient Name: <input type = text name = "name" size = 20><br><br>
+      Patient Address: <input type = text name = "address" size = 20><br><br>
+      Patient Dob: <input type = text name = "dob" size = 20><br><br>
       Patient Prioritylevel: <input type = int name "priority_level"><br><br>
       Patient Reason: <input type = string name = "reason" size = 30><br><br>
       <input type = submit name = "Submit" value = "Insert"><form>
--- src/main.php
+++ src/main.php
@@ -4,5 +4,6 @@
     <ul>
       <li><a href= "insert_Staff.php">Add a staff member</a></li>
       <li><a href= "insert_patient.php">Add a patient</a></li>
-
+      <li><a href= "insert_Room.php"> Add a new room</a></li>
+      
 </html>
