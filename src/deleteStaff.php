129f001a4bc26498ff9b72897dd421e1b3d92c94
Blake (Blake) McLachlin <mclb3660@36groot.cs.uleth.ca>
2020-03-04 19:32:32 -0700

    Got delete staff working with error handling

--- src/deleteStaff.php
+++ src/deleteStaff.php
@@ -10,33 +10,28 @@ if (isset($_COOKIE["username"])) {
     exit;
   }
 
-  $sql = "select id from STAFF where id='$_POST[id]'";
-  $result = $conn->query($sql);
+  $name = "select id from STAFF where id='$_POST[id]'";
+  $result = $conn->query($name);
   if($result->num_rows != 0) {
     $sql = "delete from STAFF where id='$_POST[id]'";
-      if ($conn->query($sql)) {
-        echo "<h3> Staff Member Deleted! </h3>";
-        $sql = "delete from PERSON where id='$_POST[id]'";
-        $conn->query($sql);
+    if($conn->query($sql)) {
+      echo "<h3> Staff member deleted!</h3>";
+      $sql = "delete from PERSON where id='$_POST[id]'";
+      $conn->query($sql);
+    } else {
+      $err = $conn->errno;
+      if ($err == 1451) {
+        echo "<p> Cannot delete </p>";
+      } else {
+        echo "<p> MySQL error code $err </p>";
       }
-      else {
-        $err = $conn->errno;
-        if($err == 1451)
-        {
-          echo "<p>Cannot delete staff due to foreign key violation</p>";
-        } else {
-          echo "<p>MySQL error code $err </p>";
-        }
-      }
-    } 
-
-  else {
-    echo "<p>Staff member with id '$_POST[id]' does not exist</p>"
+    }
+  } else {
+    echo "<p> staff dose not exist </p>";
   }
 
-  echo "<a href=\"main.php\">Return</a> to Home Page.";
-
+  echo "<a href=\"main.php\"Return</a> to Home Page.";
 } else {
-  echo "<h3> You are not logged in! </h3><p> <a href=\"index.php\">Login First</a></p>";
+  echo "<h3> you are not logged in! </h3> <a href=\"index.php\">Login first</a></p>";
 }
 ?>
