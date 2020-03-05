65c1e3183a61920c1922a4f636b7c65c1c1a3777
Nwabunor Jeff Onwuanyi <elite057@gmail.com>
2020-03-05 12:16:08 -0700

    messing around

--- src/main.php
+++ src/main.php
@@ -2,12 +2,12 @@
 <h1 style="color:orange;"> Hospital Database </h1>
   <p> Selcet a task to do <p>
     <ul>
-      <p> Staff adjustments </p>
+      <p style="color:green;"> Staff adjustments </p>
       <li><a href= "insert_Staff.php"> Add a staff member</a></li>
       <li><a href= "delete_Staff.php"> Delete a staff member</a></li>
-      <p> Patient adjustments </p>
+      <p style="color:red;"> Patient adjustments </p>
       <li><a href= "insert_patient.php"> Add a patient</a></li>
-      <p> Room and Section adjustments </p>
+      <p style="color:brown;"> Room and Section adjustments </p>
       <li><a href= "insert_Section.php"> Add a section</a></li>
       <li><a href= "delete_Section.php"> delete a section</a></li>
       <li><a href= "insert_Room.php"> Add a room to a section</a></li>
