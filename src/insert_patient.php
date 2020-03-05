377a808bb79c8be5d0bba5cfce7b8874259011fc
Nwabunor Jeff Onwuanyi <elite057@gmail.com>
2020-03-05 16:04:48 -0700

    update add patient

--- src/insert_patient.php
+++ src/insert_patient.php
@@ -7,8 +7,10 @@
       Patient Name: <input type=text name="name" size=20><br><br>
       Patient Address: <input type=text name="address" size=20><br><br>
       Patient Dob: <input type=text name="dob" size=20><br><br>
+      Patient room number: <input type=text name="room_number" size=3><br><br>
       Patient Prioritylevel: <input type=int name="priority_level"><br><br>
       Patient Reason: <input type=string name="reason" size=30><br><br>
-      <input type=submit name="Submit" value="Insert"><form>
+      <input type=submit name="Submit" value="Insert">
+    </form>
    </body>
 </html>
