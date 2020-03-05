bc6dd39bdffb0e69727f2f9616760838ef300ee5
Nwabunor Jeff Onwuanyi <elite057@gmail.com>
2020-03-04 17:37:27 -0700

    change html

--- src/insert_Person.php
+++ src/insert_Person.php
@@ -2,11 +2,11 @@
 <head><title> Hospital Database ** Add New Person </title></head>
   <body>
     <h2> Add a new person </h2>
-    <from action = "insertPerson.php" method = post>
-      Person Id: <input type = text name = "id" size = 6><br><br>
-      Person Name: <input type = text name = "name" size = 20><br><br>
-      Person Address: <input type = text name = "address" size = 20><br><br>
-      Person Dob: <input type = text name = "dob" size = 10><br><br>
-      <input type = submit name = "Submit" value = "Insert"><form>
+    <from action="insertPerson.php" method=post>
+      Person Id: <input type=text name="id" size=6><br><br>
+      Person Name: <input type=text name="name" size=20><br><br>
+      Person Address: <input type=text name="address" size=20><br><br>
+      Person Dob: <input type=text name="dob" size=10><br><br>
+      <input type=submit name="Submit" value="Insert"><form>
    </body>
 </html>
--- src/insert_patient.php
+++ src/insert_patient.php
@@ -2,14 +2,13 @@
 <head><title> Hospital Database ** Add New Patient </title></head>
   <body>
     <h2> Add a new Patient </h2>
-    <from action = "insertpatient.php" method = post>
-      Patient Id: <input type = text name = "id" size = 6><br><br>
-      Patient Name: <input type = text name = "name" size = 20><br><br>
-      Patient Address: <input type = text name = "address" size = 20><br><br>
-      Patient Dob: <input type = text name = "dob" size = 20><br><br>
-      Patient Prioritylevel: <input type = int name "priority_level"><br><br>
-      Patient Reason: <input type = string name = "reason" size = 30><br><br>
-      <input type = submit name = "Submit" value = "Insert"><form>
-
+    <from action="insertpatient.php" method=post>
+      Patient Id: <input type=text name="id" size=6><br><br>
+      Patient Name: <input type=text name="name" size=20><br><br>
+      Patient Address: <input type=text name="address" size=20><br><br>
+      Patient Dob: <input type=text name="dob" size=20><br><br>
+      Patient Prioritylevel: <input type=int name="priority_level"><br><br>
+      Patient Reason: <input type=string name="reason" size=30><br><br>
+      <input type=submit name="Submit" value="Insert"><form>
    </body>
 </html>
