db8bd830d555814c55686f6e096185cab9ae861a
Nwabunor Jeff Onwuanyi <elite057@gmail.com>
2020-03-05 16:57:42 -0700

    Update deletePatient.php

--- src/deletePatient.php
+++ src/deletePatient.php
@@ -1,31 +1,37 @@
 <?php
-
 if (isset($_COOKIE["username"])) {
+  $username = $_COOKIE["username"];
+  $password = $_COOKIE["password"];
 
-   $username = $_COOKIE["username"];
-   $password = $_COOKIE["password"];
+  $conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,'group11');
+  if($mysqli->connect_errno)
+  {
+    echo "Connection Issue";
+    exit;
+  }
 
-   $conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,'group11');
-
-   $sql = "delete from PATIENT where id='$_POST[id]'";
-   if($conn->query($sql))
-   {
-	echo "<h3> patient deleted!</h3>";
-
-   } else {
+  $name = "select id from PATIENT where id='$_POST[id]'";
+  $result = $conn->query($name);
+  if($result->num_rows != 0) {
+    $sql = "delete from PATIENT where id='$_POST[id]'";
+    if($conn->query($sql)) {
+      echo "<h3> patient deleted!</h3>";
+      $sql = "delete from PERSON where id='$_POST[id]'";
+      $conn->query($sql);
+    } else {
       $err = $conn->errno;
-      $errtxt = $conn->error;
-      if($err == 1451)
-      {
-	 echo "<h3>Cannot delete patient with $_POST[id]!</h3>";
+      if ($err == 1451) {
+        echo "<p> Cannot delete </p>";
       } else {
-        echo "you got an error code of $err. $errtxt"; 
+        echo "<p> MySQL error code $err </p>";
       }
-   }
-   echo "<br><br><a href=\"main.php\">Return</a> to Home Page.";
+    }
+  } else {
+    echo "<p> patient dose not exist </p>";
+  }
+
+  echo "<a href=\"main.php\"Return</a> to Home Page.";
 } else {
-
-   echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>";
-
+  echo "<h3> you are not logged in! </h3> <a href=\"index.php\">Login first</a></p>";
 }
 ?>
