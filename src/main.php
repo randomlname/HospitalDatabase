9260053103ab4fd6deb4e507243a4a93fb07996e
Blake (Blake) McLachlin <mclb3660@36rogue.cs.uleth.ca>
2020-03-11 13:45:32 -0600

    Added style to main.php using BootStrap4. Also linked css file to our main.php page

--- src/main.php
+++ src/main.php
@@ -1,19 +1,46 @@
-<html>
-<h1 style="color:orange;"> Hospital Database </h1>
-  <p> Selcet a task to do <p>
-    <ul>
-      <p style="color:green;"> Staff adjustments </p>
-      <li><a href= "insert_Staff.php"> Add a staff member</a></li>
-      <li><a href= "update_Staff.php"> Update a staff member</a></li>
-      <li><a href= "delete_Staff.php"> Delete a staff member</a></li>
-      <p style="color:red;"> Patient adjustments </p>
-      <li><a href= "insert_patient.php"> Add a patient</a></li>
-      <li><a href= "update_Patient.php"> update a patient</a></li>
-      <li><a href= "delete_Patient.php"> Delete a patient</a></li>
-      <p style="color:brown;"> Room and Section adjustments </p>
-      <li><a href= "insert_Section.php"> Add a section</a></li>
-      <li><a href= "delete_Section.php"> delete a section</a></li>
-      <li><a href= "insert_Room.php"> Add a room to a section</a></li>
-      <li><a href= "delete_Room.php"> delete a room from a section</a></li>
-    </ul>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="/styles/main.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+
+<body>
+  <div class="jumbotron text-center">
+    <h1> Hospital Database Application </h1>
+  </div>
+      <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+              <h3> Staff adjustments </h3>
+              <ul>
+                <li><a href= "insert_Staff.php"> Add a staff member</a></li>
+                <li><a href= "update_Staff.php"> Update records for a staff member</a></li>
+                <li><a href= "delete_Staff.php"> Delete a staff member</a></li>
+              </ul>
+            </div>
+            <div class="col-sm-4">
+              <h3> Patient adjustments </h3>
+              <ul>
+                <li><a href= "insert_patient.php"> Add a patient</a></li>
+                <li><a href= "update_Patient.php"> Update records for a patient</a></li>
+                <li><a href= "delete_Patient.php"> Delete a patient</a></li>
+              </ul>
+            </div>
+            <div class="col-sm-4">
+              <h3> Room and Section adjustments </h3>
+              <ul>
+                <li><a href= "insert_Section.php"> Add a section</a></li>
+                <li><a href= "delete_Section.php"> delete a section</a></li>
+                <li><a href= "insert_Room.php"> Add a room to a section</a></li>
+                <li><a href= "delete_Room.php"> delete a room from a section</a></li>
+              </ul>
+            </div>
+      </div>
+    </div>
+  </body>
+
 </html>
