0d875a84efdf958a3ac043a612365a3357b00593
Nwabunor Jeff Onwuanyi <elite057@gmail.com>
2020-03-12 16:16:22 -0600

    added insert folder

--- src/main.php
+++ src/main.php
@@ -17,7 +17,7 @@
             <div class="col-sm-4">
               <h3> Staff adjustments </h3>
               <ul>
-                <li><a href= "insert_Staff.php"> Add a staff member</a></li>
+                <li><a href= "insert/insert_Staff.php"> Add a staff member</a></li>
                 <li><a href= "update_Staff.php"> Update records for a staff member</a></li>
                 <li><a href= "delete_Staff.php"> Delete a staff member</a></li>
               </ul>
@@ -25,7 +25,7 @@
             <div class="col-sm-4">
               <h3> Patient adjustments </h3>
               <ul>
-                <li><a href= "insert_patient.php"> Add a patient</a></li>
+                <li><a href= "insert/insert_patient.php"> Add a patient</a></li>
                 <li><a href= "update_Patient.php"> Update records for a patient</a></li>
                 <li><a href= "delete_Patient.php"> Delete a patient</a></li>
               </ul>
@@ -33,7 +33,7 @@
             <div class="col-sm-4">
               <h3> Room and Section adjustments </h3>
               <ul>
-                <li><a href= "insert_Section.php"> Add a section</a></li>
+                <li><a href= "insert/insert_Section.php"> Add a section</a></li>
                 <li><a href= "delete_Section.php"> delete a section</a></li>
                 <li><a href= "insert_Room.php"> Add a room to a section</a></li>
                 <li><a href= "delete_Room.php"> delete a room from a section</a></li>
