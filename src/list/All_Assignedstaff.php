af2dc731bdb0cfd9430450ed897d3eb36b31fcc6
Nwabunor Jeff Onwuanyi <elite057@gmail.com>
2020-04-03 12:09:38 -0600

    Update All_Assignedstaff.php

--- src/list/All_Assignedstaff.php
+++ src/list/All_Assignedstaff.php
@@ -1,8 +1,20 @@
 <html>
-<head><title> Hospital Database ** All Staff member</title></head>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="/styles/main.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <title> Hospital Database ** All Staff member</title>
+</head>
   <body>
+    <div class="jumbotron text-center">
     <h2> Display all staff assigned to a room  </h2>
+    </div>
     <p style="color:green;"> Staff member List </p>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md">
     <?php
     echo "<strong><a href=\"../main.php\">Return</a></strong> to Home Page.";
     if(isset($_COOKIE["username"]))
@@ -33,5 +45,8 @@
       echo "<h3> You are not logged in! </h3><p> <a href=\"../index.php\">Login First</a></p>";
     }
      ?>
+   </div>
+ </div>
+ </div>
    </body>
 </html>
