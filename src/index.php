f5c09dffe37a4179d61402694396e1f054929241
Nwabunor Jeff Onwuanyi <elite057@gmail.com>
2020-04-02 20:57:43 -0600

    style test

--- src/index.php
+++ src/index.php
@@ -1,5 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
 <html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="/styles/main.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
 <body>
+  <div class="jumbotron text-center">
   <h1> Hospital Database login page </h1>
   <form action="setpass.php" method=post>
     MySQL username: <input type=text name="username" size=8>
