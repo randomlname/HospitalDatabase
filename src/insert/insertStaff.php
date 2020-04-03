13145c0e2666a3445e254ed15c3787dd281520a8
Nwabunor Jeff Onwuanyi <elite057@gmail.com>
2020-04-03 00:07:36 -0600

    Update insertStaff.php

--- src/insert/insertStaff.php
+++ src/insert/insertStaff.php
@@ -10,7 +10,7 @@ if (isset($_COOKIE["username"])) {
     exit;
   }
 
-  $sql1 = "insert int OVERSEES values ('$_POST[id]')"
+  $sql1 = "insert into OVERSEES values ('$_POST[id]')"
   $sql = "insert into PERSON values ('$_POST[id]','$_POST[name]','$_POST[address]','$_POST[dob]')";
   if ($conn->query($sql))
   {
