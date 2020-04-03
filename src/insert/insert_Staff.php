70eac8c1eddd0c2b87326d7edfcfeb8b8c3c22ea
Nwabunor Jeff Onwuanyi <elite057@gmail.com>
2020-04-02 21:40:50 -0600

    style update

--- src/insert/insert_Staff.php
+++ src/insert/insert_Staff.php
@@ -1,5 +1,13 @@
 <html>
-<head><title> Hospital Database ** Add New staff </title></head>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="/styles/main.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <div class="jumbotron text-center">
+  <title> Hospital Database ** Add New staff </title>
+  </div>
+</head>
   <body>
     <h2> Add a new staff member </h2>
     <form action="insertStaff.php" method=post>
