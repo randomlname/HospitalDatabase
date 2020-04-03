a2254084277094cca8abc8be5e39d7efd886d5f8
Blake (Blake) McLachlin <mclb3660@53maul.cs.uleth.ca>
2020-04-03 14:19:01 -0600

    change container-fluid to container

--- src/main.php
+++ src/main.php
@@ -12,7 +12,7 @@
   <div class="jumbotron text-center">
     <h1> Hospital Database Application </h1>
   </div>
-      <div class="container-fluid">
+      <div class="container">
         <div class="row">
             <div class="col-sm-4">
               <h3> Staff adjustments </h3>
@@ -48,8 +48,8 @@
                 <li><a href= "list/All_Room.php"> Show all rooms </a></li>
               </ul>
             </div>
+	</div>
       </div>
-    </div>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
